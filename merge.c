b975b761309bc19220866f60489e0687d05d8fa0
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-02-21 11:02:36 +0530

    Create merge.c

---
+++ merge.c
@@ -0,0 +1,18 @@
+void print array(int a[],int size)
+{
+int i;
+for(i=0;i<size;i++)
+printf("%d",a[i])
+printf("\n");
+}
+int main()
+{
+int arr[]={1,2,4,5,6,8}
+int arr_size=sizeof(a)/sizeof(arr[0]);
+printf("give arr");
+printarray(arr,arr_size);
+mergesort(arr,0,arr_size_1);
+printf("sort arr");
+printarray(arr,arr_size);
+return 0;
+}
